37d639995ab3d5da2a0eedba2a6f915adcf61c15
HunterJuneau <huntlee17@gmail.com>
2020-07-20 19:57:19 -0500

    made some functions!

---
+++ functions.js
@@ -0,0 +1,30 @@
+// NUGGETIZER
+// const nuggetizer = (animal) => {
+//   return `processed ${animal}`;
+// };
+
+// console.log(nuggetizer('pig'));
+// console.log(nuggetizer('fish'))
+// console.log(nuggetizer('chicken'))
+// console.log(nuggetizer('student'))
+
+
+// ADD 42
+// const addFortyTwo = (number) => {
+//   if (typeof number === "number") {
+//     return number + 42;
+//   } else {
+//     return "That's not a number!";
+//   }
+// };
+
+// console.log(addFortyTwo(56));
+// console.log(addFortyTwo("fish"));
+
+
+// AGE IN 2099
+const futureAge = (birthYear) => {
+    return 2099 - birthYear
+}
+
+console.log(futureAge(2000))
